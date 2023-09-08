74982f952a97cc02f77ee63b094b3fda04ad2785
addis <addeson3@gmail.com>
2023-09-08 14:11:35 -0700

    added some REST functionalities

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {Task, TaskStatus} from './task.model';
 import { v4 as uuid} from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable() 
 export class TasksService {
     private tasks: Task[] = []; 
@@ -9,7 +10,13 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find((task) => task.id === id)  
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const {title, description} = createTaskDto;
+
            const task: Task = {
              id: uuid(),
              title,
@@ -19,4 +26,13 @@ export class TasksService {
         this.tasks.push(task);
         return task;
     }
+
+    deleteTaskById(id: string): void{
+       this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+    upadteTaskStatus(id: string, status: TaskStatus){
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    } 
 }
